1aa20b57c90b34c658491d8e8eb45205b0c5ab73
Dakitidunno <juancasallasceballos@icloud.com>
2026-04-02 22:14:11 -0500

    Add fetching functionality for albums and state management

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,73 @@ function App() {
   const [loading, setLoading] = useState(false);
 
 
+// Дома часть,  зто дома часть
+    
+  const [дома, setДома] = useState([]);
+
+
+useEffect(() => {
+
+   async function fetchДома() {
+
+    const controller = new AbortController();
+  const signal = controller.signal;
+
+    setLoading(true);
+     setError(null);
+    
+
+
+    try {
+ 
+
+      const response = await fetch (`https://itunes.apple.com/search?term=pop&entity=album&limit=20`,{signal});
+
+      if(!response.ok){
+        throw new Error("Response was not ok")
+      }
+      const data = await response.json();
+ 
+      if(!signal.aborted){
+        setДома(data.results);
+         console.log(data)
+      }
   
+
+      
+    } catch (error) {
+      if (error.name === "AbortError"){
+        console.log("Fetch aborted");
+      }else{
+        setError(error)
+        if(!signal.aborted){
+          setError(error.message);
+        }
+      }
+    }
+
+    finally{
+      if (!signal.aborted){
+        setLoading(false);
+      }
+    }
+
+return () => { 
+    controller.abort();
+  };
+
+  }
+
+  fetchДома();
+  
+}, []);
+
+
+
+  
+
+
+//Fetch part of the input part
 
   async function fetchD(url, signal) {
 
@@ -87,16 +153,23 @@ useEffect(() => {
 
 }, [searchTerm]);
 
-
+// To see when the input fetch is made
 useEffect(() => {
   console.log(fData); 
   console.log(fData.length);
   console.log(typeof(fData));
 }, [fData]);
 
+// To see when the home fetch is made
+useEffect(() => {
+  console.log(дома); 
+  // console.log(fData.length);
+  // console.log(typeof(fData));
+}, [дома]);
+
   return (
     <>
-     <iTunesContext.Provider value={{setSearchTerm, fData}}>
+     <iTunesContext.Provider value={{setSearchTerm, fData, дома, error, loading}}>
       <FetchCompo/>
      </iTunesContext.Provider>
       
