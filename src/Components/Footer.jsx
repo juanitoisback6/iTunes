3ca285fb79abc667feec74e29656d64719ee7cbd
Dakitidunno <juancasallasceballos@icloud.com>
2026-04-05 23:33:38 -0500

    Add Footer component with GitHub link and image

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,24 @@
+
+import cj from "../assets/cj-gta.gif"
+
+
+
+
+
+
+
+export default function Footer (){
+
+ return(
+  <>
+
+   <footer className="footer">
+          <a href="https://github.com/juanitoisback6">
+<p> Made with love ❤️ by @Dakitidunno </p>
+          </a>
+  
+  <img src={cj} width="200"/>
+  </footer>
+  </>
+ )         
+}
